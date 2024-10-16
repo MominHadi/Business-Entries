fefec3c02ae0e68ccce9f6450125d1de9c3fb4b9
abdulwahid2003 <mominwahid8080@gmail.com>
2024-10-16 17:44:17 +0530

    Third

--- frontend/package/src/index.js
+++ frontend/package/src/index.js
@@ -4,18 +4,17 @@ import {createRoot} from 'react-dom/client';
 import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { HashRouter } from "react-router-dom";
+import { HashRouter,BrowserRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 
 root.render(
-  <Suspense fallback={<Loader />}>
-    <HashRouter>
+  <BrowserRouter>
       <App />
-    </HashRouter>
-  </Suspense>
+   
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
