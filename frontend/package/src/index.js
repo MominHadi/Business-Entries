08840e4c3a55df9d8c07f9dd543cd38d24b936b0
abdulwahid2003 <mominwahid8080@gmail.com>
2024-10-16 14:35:40 +0530

    Fourth

--- frontend/package/src/index.js
+++ frontend/package/src/index.js
@@ -4,9 +4,7 @@ import {createRoot} from 'react-dom/client';
 import "./assets/scss/style.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-// import { HashRouter } from "react-router-dom";
-import { BrowserRouter } from "react-router-dom";
-
+import { HashRouter } from "react-router-dom";
 import Loader from "./layouts/loader/Loader";
 
 const rootElement = document.getElementById('root');
@@ -14,9 +12,9 @@ const root = createRoot(rootElement);
 
 root.render(
   <Suspense fallback={<Loader />}>
-    <BrowserRouter>
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Suspense>
 );
 
